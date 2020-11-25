753bd8bd9f522344b6c881130855538fb272ff37
Eduardo Seije <56317467+EduardoSeije@users.noreply.github.com>
2020-11-25 16:22:30 -0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,96 @@
+import api from './api';
+
+class App{
+    constructor(){
+        this.repositories = [];
+
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository');
+        this.listEl = document.getElementById('repo-list');
+
+        this.registerHandlers() 
+        }
+
+        registerHandlers() {
+            this.formEl.onsubmit = (event) => this.addRepository(event);
+
+        }
+
+        setLoading (loading = true) {
+            if (loading === true) {
+                let loadingEl = document.createElement('span');
+                loadingEl.appendChild(document.createTextNode('Carregando'));
+                loadingEl.setAttribute('id', 'loading')
+
+                this.formEl.appendChild(loadingEl);
+
+            } else {
+                documento.getElementById('loading').remove();
+            }
+
+        }
+        async addRepository(event) {
+            event.preventDefault();
+
+            const repoInput = this.inputEl.value;
+
+            if (repoInput.length === 0)
+            return;
+
+            this.setLoading();
+
+            try {
+                const response = await api.get(`/repos/${repoInput}`);
+
+                const { name, description, html_url, owner: {avatar_url} } = response.data;
+                
+    
+    
+                this.repositories.push({
+                    name,
+                    description,
+                    avatar_url,
+                    html_url,
+                });
+    
+                this.inputEl.value = '';
+    
+                this.render();
+            } catch (err) {
+                alert('O repositório não existe')
+            }
+
+            this.setLoading(false);
+
+        }
+
+        render() {;
+        this.lisEl.innerHTML = '';
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEL = document.createElement('strong');
+            titleEL.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEL = document.createElement('p')
+            descriptionEL.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEL);
+            listItemEl.appendChild(descriptionEL);
+            listItemEl.appendChild(linkEl);
+            
+            this.listEl.appendChild(listItemEl);
+        });          
+     
+        }
+
+     }
+
+     new App();
