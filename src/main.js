2282eb3565623e2481a995e494631f0228f82efd
Eduardo Seije <56317467+EduardoSeije@users.noreply.github.com>
2020-11-25 16:21:16 -0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,74 @@
+import api from './api';
+
+class App{
+    constructor(){
+        this.repositories = [];
+
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository');
+        this.listEl = document.getElementById('repo-list');
+
+        this.registerHandlers() 
+        }
+
+        registerHandlers() {
+            this.formEl.onsubmit = (event) => this.addRepository(event);
+
+        }
+
+        async addRepository(event) {
+            event.preventDefault();
+
+            const repoInput = this.inputEl.value;
+
+            if (repoInput.length === 0)
+            return;
+
+            const response = await api.get(`/repos/${repoInput}`);
+
+            const { name, description, html_url, owner: {avatar_url} } = response.data;
+            
+
+
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
+
+            this.inputEl.value = '';
+
+            this.render();
+        }
+
+        render() {;
+        this.lisEl.innerHTML = '';
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEL = document.createElement('strong');
+            titleEL.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEL = document.createElement('p')
+            descriptionEL.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEL);
+            listItemEl.appendChild(descriptionEL);
+            listItemEl.appendChild(linkEl);
+            
+            this.listEl.appendChild(listItemEl);
+        });          
+     
+        }
+
+     }
+
+     new App();
